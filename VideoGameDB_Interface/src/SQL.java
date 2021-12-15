9359a0819f252e61748ac118d996784ae80ff215
compguy123 <77632039+compguy123@users.noreply.github.com>
2021-12-14 19:06:42 -0600

    final today

--- VideoGameDB_Interface/src/SQL.java
+++ VideoGameDB_Interface/src/SQL.java
@@ -30,8 +30,60 @@ public class SQL {
         "11.Find all the warnings for each game rating.",
         "12.Find the 5 worst pokemon game made according to user and critic score."};
 
-
-
+        sqlQueries = new String[] {"\n" +
+                "\n" +
+                "SELECT gameName, DDOff.devName, LOC.country\n" +
+                "FROM (Developed D LEFT JOIN DeveloperOffices DOff ON D.devName=DOff.devName) DDOff\n" +
+                "LEFT JOIN Locations Loc ON DDOff.locID=Loc.locID\n" +
+                "WHERE Loc.country='Canada' AND gameName IN\n" +
+                "(SELECT gameName FROM Ratings\n" +
+                "WHERE rating='E'\n" +
+                "UNION\n" +
+                "SELECT gameName FROM PublishedVideoGames\n" +
+                "WHERE genre='strategy'\n" +
+                "UNION\n" +
+                "SELECT gameName FROM Scores\n" +
+                "WHERE scoreType='Critic' AND scoreValue<=60);\n"," select gameName,platform, pubName, naSales from PublishedVideoGames natural join publishers natural join sales where gamesPublished > 10 AND gameName in( select gameName from PublishedVideoGames natural join sales natural join scores where (scoreValue/outOf > 0.5)) order by naSales Desc ;",
+        "SELECT gameName, platform, pubName, devName, (globalCount + naCount + euCount + jpCount + otherCount)/5 AS avgPlayerCount\n" +
+                "FROM\n" +
+                "PublishedVideoGames NATURAL JOIN PlayedBy NATURAL JOIN Players NATURAL JOIN Developed\n" +
+                "WHERE avgPlayerCount in\n" +
+                "(SELECT MIN((globalCount + naCount + euCount + jpCount + otherCount)/5) AS minPlayerCount\n" +
+                "FROM\n" +
+                "PublishedVideoGames NATURAL JOIN PlayedBy NATURAL JOIN Players NATURAL JOIN Developed);\n","select country,gameName,platform from Locations natural join DeveloperOffices \n" +
+                "natural join Developed where country='Japan' and gameName in(\n" +
+                "select gameName from sales where jpSales> naSales and jpSales>euSales and jpSales>otherSales ) \n","SELECT gameName, platform, euCount AS num_Of_EU_Players\n" +
+                "FROM PlayedBy PB LEFT JOIN Players P ON PB.playerGroupID=P.playerGroupID\n" +
+                "WHERE gameName IN\n" +
+                "(SELECT PVG.gameName\n" +
+                "FROM PublishedVideoGames PVG LEFT JOIN Scores S ON PVG.gameName=S.gameName\n" +
+                "WHERE S.scoreType='User' AND S.scoreValue>=8\n" +
+                "GROUP BY PVG.gameName, PVG.platform)\n" +
+                "ORDER BY euCount DESC;\n","  SELECT distinct country FROM Locations WHERE country in ( SELECT country FROM PublishedVideoGames NATURAL JOIN Developed NATURAL JOIN DeveloperOffices NATURAL JOIN Locations NATURAL JOIN Scores GROUP BY gameName, platform HAVING AVG(scoreValue/outof) > 0.8);\n","" +
+                "SELECT pubName, gameName, platform, scoreValue\n" +
+                "FROM (SELECT  * FROM PublishedVideoGames PVG LEFT JOIN Scores S ON PVG.gameName=S.gameName AND PVG.platform=S.platform\n" +
+                "WHERE scoreType='User'\n" +
+                "ORDER BY scoreValue DESC\n" +
+                "LIMIT 50)\n" +
+                "WHERE genre='Shooter';\n","select gameName,platform,naCount from PublishedVideoGames natural join playedBy natural join players where naCount > 1 And gameName like 'SoulCalibur%' ;\n",
+        "select Developed.devName, PublishedVideoGames.releaseYear, sales.globalSales,\n" +
+                "players.globalCount as numOfPlayers, Scores.scoreValue/Scores.outOf as score\n" +
+                "from Developed natural join PublishedVideoGames\n" +
+                "natural join sales natural join scores natural join PlayedBy \n" +
+                "natural join Players\n" +
+                "where releaseYear < 2000\n" +
+                "order by globalSales DESC, numOfPlayers DESC, score DESC \n" +
+                "Limit 50\n","select gameName,platform,globalCount from PublishedVideoGames natural join players natural join\n" +
+                "     PlayedBy where globalCount > 1 GROUP BY gameName HAVING count(platform)=1 ;\n" +
+                "\n","SELECT rating, MAX(drugRef) AS drugRef, MAX(blood) As blood, MAX(bloodAndGore) AS bloodAndGore, MAX(cartoonViolence) AS cartoonViolence, MAX(fantasyViolence) AS fantasyViolence, MAX(animatedBlood) AS animatedBlood, MAX(alcoholRef) AS alcoholRef, MAX(crudeHumor) AS crudeHumor\n" +
+                "FROM Ratings NATURAL JOIN GameRatings\n" +
+                "GROUP BY rating;\n","SELECT gameName, platform, AVG(scoreValue/outOf) as avg_percent\n" +
+                "FROM Scores\n" +
+                "WHERE gameName LIKE '%Pokemon%'\n" +
+                "GROUP BY gameName, platform\n" +
+                "ORDER BY avg_percent ASC\n" +
+                "LIMIT 5;\n" +
+                "\n"};
         initialize();
 
     }
